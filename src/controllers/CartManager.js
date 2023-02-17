125055b608965c4ed4362ff4cac21eadf5522177
luciano <rodriguezluciano761@gmail.com>
2023-02-17 14:54:51 -0300

    primera entrega error cart routes1

--- src/controllers/CartManager.js
+++ src/controllers/CartManager.js
@@ -1,5 +1,16 @@
-import {promises as fs} from "fs";
-const ruta = "./src/models/carts.txt"
+import {existsSync,promises as fs} from "fs";
+
+
+
+
+class Cart {
+    constructor(id, products) {
+        this.id = id;
+        this.products = products;
+    }
+}
+
+
 
 export class CartManager{
     constructor(path){
@@ -7,51 +18,76 @@ export class CartManager{
     }
    
 
-    static addId(){
-        if(this.idIncrement){
-            this.idIncrement++
-        }else{
-            this.idIncrement = 1
-        }
-        return this.idIncrement
+    checkText = () => {
+        //Creamos archivo TXT DE carrito
+        !existsSync(this.path) && fs.writeFile(this.path, "[]", 'utf-8');
     }
 
-    async addCart(cart){
-        const read = JSON.parse(await fs.readFile(this.path,"utf-8"));
-        read.push(cart)
-        await fs.writeFile(this.path, JSON.stringify(read))
-    }
 
-    async getProducts(){
+    async addCart(){
+        this.checkText()
         try{
-            const read = JSON.parse(await fs.readFile(this.path, "utf-8"))
-            return read;
+            const read = JSON.parse(await fs.readFile(this.path,"utf-8"));
+            let newId;
+            read.length > 0 ? newId = read[read.length -1].id + 1: newId = 1;
+            const newCart = new Cart (newId, []);
+            read.push(newCart)
+            await fs.writeFile(this.path, JSON.stringify(read))
+            return newId
         }catch(error){
-            return error
+            return "Hubo un error al agregar el carrito"
         }
+        
     }
-    
 
-    async getProductById(id){
-        const read = JSON.parse(await fs.readFile(this.path, "utf-8"));
-        if(read.some(prod=>prod.id === parseInt(id))){
-            return prods.find(prod => prod.id === parseInt(id))
+
+
+    async getProductById(idCart){
+        this.checkText()
+        try{
+            const read = JSON.parse(await fs.readFile(this.path, "utf-8"));
+            let cartIndex = read.findIndex(cart=> cart.id === parseInt(idCart));
+
+            if(read[cartIndex]){
+                return read[cartIndex]
+            }else{
+                return "Carrito con id puesto no encontrado"
+            }
+
+        }catch(error){
+            return "No se encontró ningún carrito"
+        }
+        
+    }
+
+
+    async addProductToCart(idCart,idProduct, prodQty){
+        this.checkText()
+        const read = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        //miramos si el carrito con ese id existe
+        if(read.some(cart=> cart.id === parseInt(idCart))){
+            //obtengo el indice del array de carrito
+            const cartIndex = read.findIndex(cart=> cart.id === parseInt(idCart))
+            //obetenemos el indice del producto dentro del carrito
+            const objectCart = new Cart(idCart, read[cartIndex].products)
+            const prodIndex = objectCart.products.findIndex(obj=>obj.product === parseInt(idProduct))
+            
+            if(prodIndex === -1){
+                //si no existe pusheamos el producto en el array de productos dentro del carrito
+                objectCart.products.push({product:idProduct, quantity:prodQty})
+                //actualizo el carrito en el array de carritos
+                read[cartIndex] = objectCart
+            }else{
+                read[cartIndex].products[prodIndex].quantity += prodQty
+            }
+
+            await fs.writeFile(this.path, JSON.stringify(read), "utf-8")
+            return "Producto agregado al carrito"
         }else{
-            return "Producto no encontrado"
+            "Hubo un error al agregar el producto al carrito"
         }
     }
 
 
+
 }
-
-
-
-
-
-
-const manager = new CartManager(ruta)
-
-const test = async()=>{
-    await fs.writeFile(ruta,"[]")
-}
- test()
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,20 +1,37 @@
 import {Router} from "express"
 import {CartManager} from "../controllers/CartManager.js"
+import { ProductManager } from "../controllers/ProductManager.js"
 
 const routerCart = Router()
-const manager = new CartManager("./src/models/carts.txt")
+const managerCart = new CartManager("./src/models/carts.txt")
+const managerProduct = new ProductManager("./src/models/products.txt")
+
+
+
 
 routerCart.post("/", async(req,res)=>{
-    const productAdd = await manager.addCart(req.body);
+    const productAdd = await managerCart.addCart()
     res.send(productAdd)
 })
 
 routerCart.get("/:cid", async(req,res)=>{
-    let idCart = await manager.getCartById(req.params.cid)
-    res.send(idCart)
+    let cart = await managerCart.getCartById(parseInt(req.params.cid))
+    res.send(cart)
 })
-routerCart.post("/:cid/product/:pid", async(req,res)=>{
 
+
+
+
+routerCart.post("/:cid/product/:pid", async(req,res)=>{
+    const prodQty = 1
+    const productData = await managerProduct.getProductById(parseInt(req.params.pid))
+
+    if(productData){
+        const data = await managerCart.addProductToCart(parseInt(req.params.cid), parseInt(req.params.pid),prodQty)
+        data ? res.send(`Producto ${productData.id} agregado al carrito`) : res.send("Hubo un error al agregar al carrito")
+    }else{
+        res.send(`El producto ${req.params.pid} no se ha encontrado`)
+    }
 })
 
 
